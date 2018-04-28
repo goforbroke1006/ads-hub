962e704dadbf90ad8b1097152f55c3c62e32336b
sergey.cherkesov <sergey.cherkasov@devim.team>
2018-04-28 15:50:52 +0300

    Fixes

--- vk-auth.py
+++ vk-auth.py
@@ -1,3 +1,4 @@
+import urllib2
 import sys
 import webbrowser
 import urlparse
@@ -8,13 +9,19 @@ from os.path import expanduser
 
 print(sys.argv)
 
+token_file_path = expanduser("~") + "/.adv-hub/vk-token"
+
 http_port = int(sys.argv[1])
-client_id = sys.argv[2]
+client_id = int(sys.argv[2])
+client_secret = sys.argv[3] if len(sys.argv) >= 4 else None
 redirect_uri = "http://localhost:%d/app_dev.php/login/check-vkontakte" % http_port
 
 url = "https://oauth.vk.com/authorize" \
-      "?client_id=%s&redirect_uri=%s&display=popup&scope=offline,ads&response_type=token&v=5.74&state=123456" \
+      "?client_id=%s&redirect_uri=%s&display=popup&scope=ads&response_type=token&v=5.74&state=123456" \
       % (client_id, redirect_uri)
+#url = "https://oauth.vk.com/authorize?client_id=%d&display=popup&redirect_uri=%s&scope=offline,ads&response_type=code&v=5.74" \
+#    % (client_id, redirect_uri)
+print url
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from multiprocessing import Process
@@ -27,8 +34,21 @@ class VkAuthServer(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        self._set_headers()
+        print self.path
+        if len(self.path.split("?")) == 2:
+            data_dict = dict(urlparse.parse_qsl(self.path.split("?")[1]))
+            if "code" in data_dict:
+                token_url = "https://oauth.vk.com/access_token?client_id=%d&client_secret=%s&redirect_uri=&code=%s" \
+                    % (client_id, client_secret, params["code"])
+                res = urllib2.urlopen(token_url)
 
+                f = open(token_file_path, 'w+')
+                f.write(res)
+                f.close()
+
+                os._exit(0)
+
+        self._set_headers()
         self.wfile.write(
             "<html>"
             + "<body>"
@@ -46,15 +66,13 @@ class VkAuthServer(BaseHTTPRequestHandler):
             + "</html>")
 
     def do_POST(self):
-        # self._set_headers()
-
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
 
         data_dict = dict(urlparse.parse_qsl(post_data))
         print data_dict
 
-        f = open(expanduser("~") + '/.adv-hub/vk-token', 'w+')
+        f = open(token_file_path, 'w+')
         f.write(json.dumps(data_dict))
         f.close()
 
--- vk-import.py
+++ vk-import.py
@@ -12,19 +12,39 @@ cfg = json.load(f)
 ids_types = ["ad", "campaign", "client", "office",]
 
 account_id = int(sys.argv[1])
-ids_type = sys.argv[2]
-if ids_type not in ids_types:
-    raise Exception("Unexpected ids_type: " + ids_type + ". Try to use next - " + str(ids_types).strip('[]'))
+#ids_type = sys.argv[2]
+#if ids_type not in ids_types:
+#    raise Exception("Unexpected ids_type: " + ids_type + ". Try to use next - " + str(ids_types).strip('[]'))
 
-url = "https://api.vk.com/method/ads.getStatistics?access_token=%saccount_id=%d&ids_type=%s&ids=&period=day&date_from=%s&date_to=%s" % (
-        cfg["access_token"], account_id, ids_type,
-        None, datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
-    )
+#url = "https://api.vk.com/method/ads.getStatistics?access_token=%saccount_id=%d&ids_type=%s&ids=&period=day&date_from=%s&date_to=%s" % (
+#        cfg["access_token"], account_id, ids_type,
+#        None, datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
+#    )
+
+url = "https://api.vk.com/method/ads.getAdsLayout?access_token=%s&account_id=%d&include_deleted=1&v=5.74" % (cfg["access_token"], account_id)
+print url
 
 try:
     response = urllib2.urlopen(url)
     data = json.load(response)
-    print data
+
+    #f = open("./qwere.txt", 'w+')
+    #f.write(str(data))
+    #f.close()
+
+    #print data
+    for row in data["response"]:
+        print row["age_restriction"]
+        print row["ad_format"]
+        print row["title"]
+        print row["link_url"]
+        print row["preview_link"]
+        print row["campaign_id"]
+        print row["image_src"]
+        print row["cost_type"]
+        print row["link_domain"]
+        print row["id"]
+        print "========== ========== ========== ========== =========="
 except urllib2.HTTPError as ex:
     if ex.code == 400:
         print "You have not access for this account data"
