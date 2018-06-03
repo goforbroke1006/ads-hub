f699e7d859785480f17e86a25945a89cba2b8827
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-06-04 00:53:10 +0300

    Fix

--- script/server/task/fb-import.py
+++ script/server/task/fb-import.py
@@ -1,19 +1,29 @@
 import json
 from os.path import expanduser
 
-import facebookads
+from facebookads import FacebookAdsApi
 from facebookads.adobjects.adaccount import AdAccount
 from facebookads.adobjects.adaccountuser import AdAccountUser
+from facebookads.adobjects.adspixel import AdsPixel
+from facebookads.objects import Page
 
 auth_config = json.load(
     open(expanduser("~") + '/.ads-hub/fb-token', 'r')
 )
 
-app_id = auth_config["app_id"]
-app_secret = auth_config["app_secret"]
+# https://developers.facebook.com/docs/marketing-api/insights-api
+
+# app_id = auth_config["app_id"]
+# app_secret = auth_config["app_secret"]
 access_token = auth_config["access_token"]
 
-facebookads.FacebookAdsApi.init(app_id, app_secret, access_token)
+FacebookAdsApi.init(access_token=access_token)
+
+# print AdsPixel('772856039569351').get_ad_accounts(1034901646550640)
+# print AdsPixel('772856039569351').get_stats()
+
+# page = Page(122961418485088)
+# print page.get_leadgen_forms()
 
 account = AdAccount('act_1868848106489319')
 users = account.get_users()
